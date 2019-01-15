e8dacae7c520a35bb9ce02c1e903c5b8496bc372
Rihards Freibergs <43142491+rfreibergs@users.noreply.github.com>
2019-01-15 17:50:43 +0200

    Add files via upload

---
+++ labors4.py
@@ -0,0 +1,25 @@
+import math as math4
+
+#precizitate
+h = 0.01
+
+#definejam
+def f(x):
+    return math4.sin(x)
+def atv(c):
+    return (math4.sin(c+h)-math4.sin(c))/h
+def atv2(d):
+    return (math4.sin(d+h)-2*math4.sin(d)+math4.sin(d-h))/(h**2)
+
+#inputs   
+print("Tabula ar pirmo un otro sinusa atvasinajumu\n")
+a = int(input("Ievadi aprekinu sakumpunktu: "))
+b = int(input("Ievadi aprekinu beigu punktu: "))
+
+#neliels loops
+print()
+print("\t\t sinuss \tatvasinajums\totras kartas")
+print("skaitlis a\t punkta a\tpunkta a\tatvasinajums")
+print()
+for i in range(a,b+1):
+    print ("%d\t\t %.2f\t\t %.2f\t\t %.2f"%(i, f(i), atv(i), atv2(i)))
